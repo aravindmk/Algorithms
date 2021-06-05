ea4722a1567dd592817c074c85afccbd0fc24323
d4rk3n <aravindmk@outlook.com>
2021-06-05 18:45:12 +0530

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,18 +1,18 @@
 # Binary Search
 
-lst=eval(input())
+lst = eval(input())
 lst.sort()
-n=int(input())
-beg=0
-end=len(lst)-1
-while beg<=end:
-    mid=(beg+end)//2
-    if lst[mid]==n:
-        print("Found at position : ",mid+1)
+n = int(input())
+beg = 0
+end = len(lst) - 1
+while beg <= end:
+    mid = (beg + end) // 2
+    if lst[mid] == n:
+        print("Found at position : ",mid + 1)
         break
-    elif lst[mid]<n:
-        beg=mid+1
+    elif lst[mid] < n:
+        beg = mid + 1
     else:
-        end=mid-1
+        end = mid - 1
 else:
     print("Not Found")
